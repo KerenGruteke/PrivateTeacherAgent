cae04d697df944b5557262e81110bd5529d8dfd0
KerenGruteke <gkeren@campus.technion.ac.il>
2025-07-17 10:40:43 +0300

    micro agents

---
+++ src/utils/constants.py
@@ -0,0 +1,8 @@
+import os
+
+OPENAI_API_KEY = os.getenv("API_KEY")  # API key
+CHAT_DEPLOYMENT_NAME = "team9-gpt4o"
+EMBEDDING_DEPLOYMENT_NAME = "team9-embedding"
+
+AZURE_OPENAI_ENDPOINT = "?"
+API_VERSION = "?"
