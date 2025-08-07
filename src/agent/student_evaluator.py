df92b73398bf8f9fbd8a66c6636ec2da9de0bced
or1dado <ordado96@gmail.com>
2025-08-07 14:14:41 +0300

    Create a db

---
+++ src/agent/student_evaluator.py
@@ -0,0 +1,10 @@
+
+
+class StudentEvaluator:
+
+    def get(self, student_id, topic):
+        pass
+
+    def update_student_level(self, student_id, topic, current_session_feedback):
+        pass
+
